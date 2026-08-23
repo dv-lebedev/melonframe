023045c375c46253ee7aa2778e96b50b39381211
dv-lebedev <dv.lebedev777@gmail.com>
2026-08-23 18:52:07 +0300

    refact crc16 params

--- melonframe_crc.c
+++ melonframe_crc.c
@@ -1,9 +1,9 @@
 #include "melonframe_crc.h"
 #include <stdint.h>
 
-uint16_t crc16(const uint8_t *data, size_t offset, size_t size) {
+uint16_t crc16(const uint8_t *data, const size_t offset, const size_t data_len) {
     uint16_t crc = 0xFFFF;
-    for (size_t i = offset; i < size; i++) {
+    for (size_t i = offset; i < data_len; i++) {
         crc ^= (uint16_t)data[i];
         for (int b = 0; b < 8; b++) {
             if (crc & 0x0001) {
--- melonframe_crc.h
+++ melonframe_crc.h
@@ -3,6 +3,6 @@
 
 #include <stdint.h>
 
-uint16_t crc16(const uint8_t *data, size_t offset, size_t size);
+uint16_t crc16(const uint8_t *data, size_t offset, size_t data_len);
 
 #endif //MELONFRAME_MELONFRAME_CRC_H
