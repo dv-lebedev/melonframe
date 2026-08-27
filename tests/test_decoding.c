fd1a22b8fd172db4b214e5435603f5d15d905769
dv-lebedev <dv.lebedev777@gmail.com>
2026-08-27 18:23:34 +0300

    add CTest

---
+++ tests/test_decoding.c
@@ -0,0 +1,176 @@
+/*
+*    MIT License
+ *
+ *    Copyright (c) 2026 Denis Lebedev
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to deal
+ *    in the Software without restriction, including without limitation the rights
+ *    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *
+ *    The above copyright notice and this permission notice shall be included in all
+ *    copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *    SOFTWARE.
+ */
+
+#include <io.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "../melonframe.h"
+
+static int32_t write_repeated_encoded_packages(const uint16_t package_size, const uint64_t packages_count, const char *filepath) {
+    uint8_t test[package_size];
+    uint8_t counter = 0;
+    for (uint16_t i = 0; i < package_size; i++) {
+        test[i] = counter++;
+    }
+
+    FILE *f = fopen(filepath, "wb");
+
+    size_t encoded_size = 0;
+    const melonframe_result_t res = melonframe_get_size_for_encoded(sizeof(test), &encoded_size);
+    if (res != MELONFRAME_OK) {
+        printf("melonframe_decoder_process_byte returned %d\n, encoded_size: %llu\n", res, encoded_size);
+        fclose(f);
+        return -1;
+    }
+
+    uint8_t encoded[encoded_size];
+    melonframe_encode(test, package_size, encoded, encoded_size);
+
+    for (int i = 0; i < packages_count; i++) {
+        fwrite(encoded, encoded_size, 1, f);
+    }
+
+    fclose(f);
+    return 0;
+}
+
+static void handler(void *ctx, melonframe_decoder_event_t status, uint8_t *data, const size_t data_len) {
+    if (status == MELONFRAME_STATUS_NEW_PACKET) {
+        FILE *f = (FILE*)ctx;
+        fwrite(data, sizeof(uint8_t),data_len, f);
+
+        for (int i = 0; i < data_len; i++) {
+            //printf("data[%i]: %i\n", i, data[i]);
+        }
+
+    } else {
+        printf("handler melonframe_decoder_event_t returned %d\n", status);
+    }
+}
+
+static int32_t decode_and_write_encoded_packages(const char *encoded_filepath, const char *decoded_filepath) {
+    FILE *encoded_file = fopen(encoded_filepath, "rb");
+    if (encoded_file == NULL) {
+        printf("encoded_file == NULL");
+        return -1;
+    }
+
+    FILE *decoded_file = fopen(decoded_filepath, "wb");
+    if (decoded_file == NULL) {
+        fclose(encoded_file);
+        printf("decoded_file == NULL");
+        return -1;
+    }
+
+    melonframe_decoder_t decoder;
+    melonframe_buffer_t decoder_buffer = {
+        .data = malloc(sizeof(uint8_t) * 1024),
+        .size = 1024,
+    };
+    melonframe_decoder_event_t decoder_event;
+    melonframe_decoder_init(&decoder, &decoder_buffer, handler, (void*)decoded_file);
+
+    uint8_t buffer[1024];
+    while (1) {
+        const size_t read = fread(buffer, sizeof(uint8_t), 6, encoded_file);
+        if (read == 0) {
+            break;
+        }
+
+        for (int i = 0; i < read; i++) {
+            const melonframe_result_t res = melonframe_decoder_process_byte(&decoder, buffer[i], &decoder_event);
+            if (res != MELONFRAME_OK) {
+                printf("melonframe_decoder_process_byte returned %d\n", res);
+                fclose(decoded_file);
+                fclose(encoded_file);
+                return -1;
+            }
+        }
+    }
+
+    fclose(decoded_file);
+    fclose(encoded_file);
+    return 0;
+}
+
+static int32_t compare_files(const char *encoded_filepath, const char *decoded_filepath) {
+    FILE *encoded_file = fopen(encoded_filepath, "rb");
+    FILE *decoded_file = fopen(decoded_filepath, "rb");
+    if (encoded_file == NULL || decoded_file == NULL) {
+        printf("encoded_file == NULL");
+        return -1;
+    }
+
+    uint8_t enc_buffer[1024];
+    uint8_t dec_buffer[1024];
+
+    while (1) {
+        const size_t enc = fread(enc_buffer, sizeof(uint8_t), 1024, encoded_file);
+        const size_t dec = fread(dec_buffer, sizeof(uint8_t), 1024, decoded_file);
+        if (enc != dec) {
+            printf("compare_files: %llu != %llu\n", enc, dec);
+            fclose(encoded_file);
+            fclose(decoded_file);
+            return -1;
+        }
+
+        if (enc == 0 && dec == 0) {
+            printf("compare_files: files have same data.\n");
+            return 0;
+        }
+
+        size_t len = enc < dec ? enc : dec;
+        for (size_t i = 0; i < len; i++) {
+            if (enc_buffer[i] != dec_buffer[i]) {
+                printf("compare_files: pos: %llu vals: %i != %i\n", i, enc_buffer[i], dec_buffer[i]);
+                fclose(encoded_file);
+                fclose(decoded_file);
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+
+    const char *encoded_filepath = "encoded.dat";
+    const char *decoded_filepath = "decoded.dat";
+
+    int32_t res = write_repeated_encoded_packages(10, 65535, encoded_filepath);
+    if (res != 0) {
+        printf("write_repeated_encoded_packages returned %d\n", res);
+        return -1;
+    }
+
+    res = decode_and_write_encoded_packages(encoded_filepath, decoded_filepath);
+    if (res != 0) {
+        printf("decode_and_write_encoded_packages returned %d\n", res);
+        return -1;
+    }
+
+    return compare_files(encoded_filepath, decoded_filepath);
+}
--- tests/test_encoding.c
+++ tests/test_encoding.c
@@ -22,15 +22,52 @@
  *    SOFTWARE.
  */
 
-#ifndef MELONFRAME_ENCODING_TEST_H
-#define MELONFRAME_ENCODING_TEST_H
 #include <stdint.h>
-#include <stddef.h>
+#include <stdio.h>
 
-void simple_encode_test();
+#include "../melonframe.h"
 
-void write_repeated_encoded_packages(const uint16_t package_size, const uint64_t packages_count, const char *filepath);
-void decode_and_write_encoded_packages(const char *encoded_filepath, const char *decoded_filepath);
+static int test_encoding() {
+    const uint8_t arr[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x10};
 
-#endif //MELONFRAME_ENCODING_TEST_H
+    size_t encoded_size;
+    melonframe_result_t err = melonframe_get_size_for_encoded(sizeof(arr), &encoded_size);
+    if (err != MELONFRAME_OK) {
+        printf("melonframe_get_size_for_encoded: %d", err);
+        return -1;
+    }
 
+    uint8_t encoded[encoded_size];
+    err = melonframe_encode(
+        arr,
+        sizeof(arr),
+        encoded,
+        encoded_size);
+
+    if (err != MELONFRAME_OK) {
+        printf("melonframe_encode: %d", err);
+        return -1;
+    }
+
+    printf("simple_encode_test result:\n");
+    for (size_t i = 0; i < encoded_size; i++) {
+        printf("arr[%zu]:%02X\n", i, encoded[i]);
+    }
+
+    const uint8_t expected[] = {
+        0xAA, 0x55, 0x00, 0x07, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x10, 0xF6, 0x31
+    };
+
+    for (size_t i = 0; i < sizeof(expected); i++) {
+        if (encoded[i] != expected[i]) {
+            printf("encoded[i] != expected[i]:%02X != %02X\n", encoded[i], expected[i]);
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+    return test_encoding();
+}
